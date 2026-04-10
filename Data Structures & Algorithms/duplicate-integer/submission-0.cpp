ed2d68b3919f1d7250f7255102087519870bf0ea
Keval Bhavsar <86193363+nuclearshadow@users.noreply.github.com>
2026-04-10 13:19:35 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        set<int> seen;
+        for (int num : nums) {
+            if (seen.contains(num)) return true;
+            seen.insert(num);
+        }
+        return false;
+    }
+};
